4498ce0de12a69c6163daf8ce97f7966dce3f060
tlam67 <tlamottawa@gmail.com>
2021-01-11 22:41:11 -0500

    implemented bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,12 @@
+import random
+
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+
+test = random.sample(range(512), 512)
+print(bubbleSort(test))
