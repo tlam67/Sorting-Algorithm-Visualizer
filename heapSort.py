0e0326238ddcb5c19a3c13f6cc9d390efe10dbc6
tlam67 <tlamottawa@gmail.com>
2021-01-12 02:03:36 -0500

    added heapSort

---
+++ heapSort.py
@@ -0,0 +1,31 @@
+import random
+
+def createHeap(array, heapSize, index):
+    largest = index
+    left = 2 * index + 1
+    right = 2 * index + 2
+
+    if left < heapSize and array[largest] < array[left]:
+        largest = left
+
+    if right < heapSize and array[largest] < array[right]:
+        largest = right
+    
+    if largest != index:
+        array[index], array[largest] = array[largest], array[index]
+
+        createHeap(array, heapSize, largest)
+
+def heapSort(array):
+    heapSize = len(array)
+
+    for i in range(heapSize//2 - 1, -1, -1):
+        createHeap(array, heapSize, i)
+
+    for i in range(heapSize-1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        createHeap(array, i, 0)
+
+test = random.sample(range(512), 512)
+heapSort(test)
+print(test)
