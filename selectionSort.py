34f4f048bb90e231995a933d04d724a582d703cf
tlam67 <tlamottawa@gmail.com>
2021-01-12 01:33:58 -0500

    added selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,4 +1,13 @@
 import random
 
 def selectionSort(array):
-    
+    for i in range(len(array)):
+        index = i
+        for j in range(i+1, len(array)):
+            if array[index] > array[j]:
+                index = j
+        array[i], array[index] = array[index], array[i]
+    return array
+
+test = random.sample(range(512), 512)
+print(selectionSort(test))
